564a61c953af45e0280312ea1ef7fc6e2240ad7c
igprasad09 <vivek87228@gmail.com>
2025-09-24 22:07:35 -0500

    make changes in vercel an origin set to *

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(cors({
   credentials: true,
-  origin: process.env.CLIENT_URL || "https://your-frontend-domain.com",
+  origin: "*",
 }));
 
 app.use(cookieParser());
